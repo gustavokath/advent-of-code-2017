21ea15860ff8616234e8ebb3b683d1e5b3ddd664
Gustavo Kath <gustavo.kath@acad.pucrs.br>
2017-12-17 15:59:10 -0200

    Improved 14A Execution to O(1) operations

--- 16A/main.rb
+++ 16A/main.rb
@@ -1,32 +1,40 @@
-def exchange(array, a, b)
+def exchange(array, a, b, start, len, map)
   aux = array[a]
   array[a] = array[b]
   array[b] = aux
-end
-
-def partner(array, a, b)
-  a_index = array.find_index(a)
-  b_index = array.find_index(b)
-
-  exchange(array, a_index, b_index)
+  map[array[a]] = a
+  map[array[b]] = b
 end
 
 array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
+map = {
+  'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6,
+  'h' => 7, 'i' => 8, 'j' => 9, 'k' => 10, 'l' => 11, 'm' => 12, 'n' => 13, 'o' => 14, 'p' => 15
+}
+
+#map = { 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4 }
 #array = ['a', 'b', 'c', 'd', 'e']
+
 cmds = $stdin.readline.chomp.split(',')
 
+start = 0
+len = array.length
 for cmd in cmds
-  p array
   case cmd[0]
   when 's'
     num = cmd[/\d+/].to_i
-    array = array.rotate(num * -1)
+    start = start - num
+    start = len - start.abs if start < 0
   when 'x'
     cmd_nums = cmd[1..-1].split('/').map(&:to_i)
-    exchange(array, cmd_nums[0], cmd_nums[1])
+    exchange(array, (start + cmd_nums[0]) % len, (start + cmd_nums[1]) % len, start, len, map)
   when 'p'
-    partner(array, cmd[1], cmd[3])
+    exchange(array, map[cmd[1]], map[cmd[3]], start, len, map)
   end
 end
-p array
-p array.join('')
+
+result = ''
+for i in 0..(len-1)
+  result += array[(start + i) % len]
+end
+p result
