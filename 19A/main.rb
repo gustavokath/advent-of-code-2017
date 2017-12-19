c632ef6416a18ec7e1c5f6e4f63582a42533fee8
Gustavo Kath <gustavo.kath@acad.pucrs.br>
2017-12-19 20:25:14 +0000

    Improve code

--- 19A/main.rb
+++ 19A/main.rb
@@ -1,27 +1,13 @@
 def get_next(matrix,x, y, dir_x, dir_y, change)
-  return nil, nil, false if matrix[y][x] == ' '
   if change
-    if  dir_x == 0 && x < @len_x-1 && matrix[y][x+1] != ' '
-      return 1, 0, false
-    end
-    if  dir_x == 0 && x > 0 && matrix[y][x-1] != ' '
-      return -1, 0, false
-    end
-
-    if  dir_y == 0 && y < @len_y-1 && matrix[y+1][x] != ' '
-      return 0, 1, false
-    end
-    if dir_y == 0 && y > 0 && matrix[y-1][x] != ' '
-      return 0, -1, false
-    end
+    return 1, 0  if dir_x == 0 && x < @len_x-1 && matrix[y][x+1] != ' '
+    return -1, 0 if dir_x == 0 && x > 0 && matrix[y][x-1] != ' '
+    return 0, 1  if dir_y == 0 && y < @len_y-1 && matrix[y+1][x] != ' '
+    return 0, -1 if dir_y == 0 && y > 0 && matrix[y-1][x] != ' '
   else
-    if matrix[y+dir_y][x+dir_x] == '+'
-      return dir_x, dir_y, true
-    else
-      return dir_x, dir_y, false
-    end
+    return dir_x, dir_y if matrix[y][x] != ' '
   end
-  return nil, nil, false
+  return nil, nil
 end
 
 matrix = []
@@ -29,9 +15,9 @@ x = 0
 y = 0
 dir_x = 0
 dir_y = 1
-next_step = 'down'
 path = ''
 change = false
+count = 0
 
 $stdin.each_line do |line|
   line = line.split('')
@@ -43,11 +29,9 @@ end
 x = matrix[0].find_index('|')
 
 while dir_x != nil && dir_y != nil
-  if matrix[y][x] != '|' && matrix[y][x] != '-' && matrix[y][x] != '+' && matrix[y][x] != ' '
-    path += matrix[y][x]
-  end
+  path += matrix[y][x] if matrix[y][x] =~ /[A-Z]/
 
-  dir_x, dir_y, change = get_next(matrix, x, y, dir_x, dir_y, change)
+  dir_x, dir_y = get_next(matrix, x, y, dir_x, dir_y, matrix[y][x] == '+')
   unless dir_x.nil?
     x += dir_x
     y += dir_y
